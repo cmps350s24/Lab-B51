c2ca340a2f2455afc8f2a49e5ac96db2cb3c345c
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-03-04 10:57:54 +0300

    up

--- Lab Material/Lab 6- JS OOP/Tutorials/banking-app/app.js
+++ Lab Material/Lab 6- JS OOP/Tutorials/banking-app/app.js
@@ -1,4 +1,28 @@
 // import the bank
-import { add } from './repo/bank.js';
+// import {add , getAccount , deposit, withdraw} as repo from './repo/bank.js';
+import * as repo from './repo/bank.js';
 
-console.log("Hello");
+// add a new account
+const newAccount = {
+    accountNo: 777,
+    balance: 1000,
+    type: 'savings'
+}
+
+repo.add(newAccount);
+
+console.log(repo.getAccounts());
+
+repo.deposit(777, 2000);
+console.log(repo.getAccounts());
+
+repo.withdraw(777, 500);
+console.log(repo.getAccounts());
+
+repo.deleteAccount(777);
+console.log(repo.getAccounts());
+
+
+
+
+
--- Lab Material/Lab 6- JS OOP/Tutorials/banking-app/repo/bank.js
+++ Lab Material/Lab 6- JS OOP/Tutorials/banking-app/repo/bank.js
@@ -22,7 +22,54 @@ const accounts = [
 ]
 
 // functions and export them
-
+export function getAccounts() {
+    return accounts;
+}
 export function add(account) {
     accounts.push(account);
+}
+export function deposit(accountNo, amount) {
+    const index = accounts.findIndex(a => a.accountNo == accountNo)
+    if (index >= 0)
+        accounts[index].balance += amount;
+    else
+        console.log('Account not found');
+}
+export function withdraw(accountNo, amount) {
+    const index = accounts.findIndex(a => a.accountNo == accountNo)
+    if (index >= 0 && accounts[index].balance >= amount)
+        accounts[index].balance -= amount;
+    else
+        console.log('Account not found or not enough balance');
+}
+
+export function getAccount(accountNo) {
+    return account.find(a => a.accountNo == accountNo)
+}
+
+export function deleteAccount(accountNo) {
+    const index = accounts.findIndex(a => a.accountNo == accountNo)
+    if (index >= 0)
+        accounts.splice(index, 1);
+    else
+        console.log('Account not found');
+}
+
+export function sumBalance() {
+    return accounts.reduce((acc, curr) => acc + curr.balance, 0)
+}
+
+export function averageBalance() {
+    return sumBalance() / accounts.length
+}
+
+export function distributeBenefits(benefitPercentage) {
+    accounts.forEach(a => a.balance += a.balance * benefitPercentage / 100)
+}
+
+export function toJson() {
+    return JSON.stringify(accounts);
+}
+export function fromJson(accountsJSON) {
+    return JSON.parse(accountsJSON);
 }
