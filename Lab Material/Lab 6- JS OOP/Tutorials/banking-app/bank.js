a109c52b921f4d727fbb8ef535f1a75c4de6506e
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-03-04 09:40:17 +0300

    Create bank.js

---
+++ Lab Material/Lab 6- JS OOP/Tutorials/banking-app/bank.js
@@ -0,0 +1,22 @@
+const accounts = [
+    {
+        accountNo: 123,
+        balance: 500,
+        type: 'savings'
+    },
+    {
+        accountNo: 234,
+        balance: 4000,
+        type: 'current'
+    },
+    {
+        accountNo: 345,
+        balance: 35000,
+        type: 'current'
+    },
+    {
+        accountNo: 456,
+        balance: 60000,
+        type: 'savings'
+    },
+]
