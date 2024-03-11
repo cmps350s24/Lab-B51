a2d1c5c5a8339b9b6380f3436ebbc9361535dc2e
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-03-11 11:30:50 +0300

    Update index.js

--- Lab Material/Lab 7 Client JS/Tutorial/Country Explorer/js/index.js
+++ Lab Material/Lab 7 Client JS/Tutorial/Country Explorer/js/index.js
@@ -1,10 +1,23 @@
-// get the references to the elements 
+// URLS [Routes to get the data]
+const regionURL = "https://restcountries.com/v3.1/region/"
+const countryURL = "https://restcountries.com/v3.1/name/"
 
+
+// get the references to the elements 
 const regionDD = document.querySelector("#region");
 const countryDD = document.querySelector("#country");
+const factsArea = document.querySelector("#facts-area");
 
 regionDD.addEventListener("change", handleRegionChange)
 
-function handleRegionChange() {
-    alert('Hey you did make a change')
+async function handleRegionChange() {
+    const url = `${regionURL}${regionDD.value}`
+    const data = await fetch(url)
+    const countries = await data.json()
+
+    countryDD.innerHTML = countries
+        .map(country => `<option value = ${country.name.common}> ${country.name.common}</option>`)
+        .join(' ')
+
+
 }
