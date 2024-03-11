bf8fbdb5792aa1ee367aa07c7e123289bf60276d
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-03-11 11:53:42 +0300

    Update index.js

--- Lab Material/Lab 7 Client JS/Tutorial/Country Explorer/js/index.js
+++ Lab Material/Lab 7 Client JS/Tutorial/Country Explorer/js/index.js
@@ -3,12 +3,13 @@ const regionURL = "https://restcountries.com/v3.1/region/"
 const countryURL = "https://restcountries.com/v3.1/name/"
 
 
-// get the references to the elements 
+// get the references to the elements
 const regionDD = document.querySelector("#region");
 const countryDD = document.querySelector("#country");
 const factsArea = document.querySelector("#facts-area");
 
 regionDD.addEventListener("change", handleRegionChange)
+countryDD.addEventListener("change", handleCountryChange)
 
 async function handleRegionChange() {
     const url = `${regionURL}${regionDD.value}`
@@ -16,8 +17,42 @@ async function handleRegionChange() {
     const countries = await data.json()
 
     countryDD.innerHTML = countries
-        .map(country => `<option value = ${country.name.common}> ${country.name.common}</option>`)
+        .map(country => `<option value=${country.name.common}> ${country.name.common}</option>`)
         .join(' ')
 
 
+}
+
+async function handleCountryChange() {
+    const url = `${countryURL}${countryDD.value}`
+    const data = await fetch(url)
+    const countries = await data.json()
+    const country = countries[0]
+
+    factsArea.innerHTML = `
+<h1>Facts about ${country.name.common}</h1>
+<img src=${country.flags.png}>
+<table>
+    <tr>
+        <th>Official Country Name</th>
+        <td>${country.name.official}</td>
+    </tr>
+    <tr>
+        <th>Capital City</th>
+        <td>${country.name.official}</td>
+    </tr>
+    <tr>
+        <th>Currencies</th>
+        <td>${country.name.official}</td>
+    </tr>
+    <tr>
+        <th>Population</th>
+        <td>${country.name.official}</td>
+    </tr>
+    <tr>
+        <th>Languages</th>
+        <td>${country.name.official}</td>
+    </tr>
+</table>
+`
 }
