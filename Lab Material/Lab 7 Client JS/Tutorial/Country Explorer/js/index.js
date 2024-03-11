90956c33c6f1820ca736de676e58eeace30495c1
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-03-11 10:54:40 +0300

    up

--- Lab Material/Lab 7 Client JS/Tutorial/Country Explorer/js/index.js
+++ Lab Material/Lab 7 Client JS/Tutorial/Country Explorer/js/index.js
@@ -1,3 +1,10 @@
 // get the references to the elements 
 
 const regionDD = document.querySelector("#region");
+const countryDD = document.querySelector("#country");
+
+regionDD.addEventListener("change", handleRegionChange)
+
+function handleRegionChange() {
+    alert('Hey you did make a change')
+}
