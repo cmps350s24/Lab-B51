618c361d534431137c09e94c5a424924aadd2fac
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-04-01 12:05:30 +0300

    up

--- Lab Material/Lab 9– Web Pages using Next.js and React/Tutorial/lab08-bank-app/app/accounts/Accounts.jsx
+++ Lab Material/Lab 9– Web Pages using Next.js and React/Tutorial/lab08-bank-app/app/accounts/Accounts.jsx
@@ -1,17 +1,39 @@
 import React from 'react'
+import styles from '@/app/page.module.css'
 
 export default function Accounts({ initialAccounts }) {
     return (
         <div>
-            <h2>Accounts</h2>
-            <table>
-                {
-                    initialAccounts.map(account => <tr>
-                        <td>{account.accountNo}</td>
-                        <td>{account.balance}</td>
-                    </tr>)
-                }
+
+            <table id="accounts" className={styles.table}>
+                <thead>
+                    <tr>
+                        <th>Account No</th>
+                        <th>Account Type</th>
+                        <th>Balance</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        initialAccounts.map(acct => <tr id="row-${acct.accountNo}">
+                            <td>{acct.accountNo}</td>
+                            <td>{acct.acctType}</td>
+                            <td>{acct.balance}</td>
+                            <td>
+                                {acct.balance <= 0 ?
+                                    <button>
+                                        <i class="fas fa-trash"></i>
+                                    </button>
+                                    : ''
+                                }
+                            </td>
+                        </tr>
+                        )
+                    }
+                </tbody>
             </table>
+
         </div>
     )
 }
