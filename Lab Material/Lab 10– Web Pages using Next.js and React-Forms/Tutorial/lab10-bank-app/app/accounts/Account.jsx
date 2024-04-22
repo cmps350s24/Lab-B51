b8bb83330a927db4f954d430b8893d123fbb8165
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-04-22 10:42:41 +0300

    updated

--- Lab Material/Lab 10– Web Pages using Next.js and React-Forms/Tutorial/lab10-bank-app/app/accounts/Account.jsx
+++ Lab Material/Lab 10– Web Pages using Next.js and React-Forms/Tutorial/lab10-bank-app/app/accounts/Account.jsx
@@ -25,11 +25,17 @@ export default function Account({ account, onDelete }) {
                         <i class="fas fa-trash">Delete</i>
                     </button> : ''}
 
-                <button className={styles.btnEdit}>
-                    <i className="fas fa-edit">
-                        Edit
-                    </i>
-                </button>
+                <Link href={
+                    {
+                        pathname: "/accounts/upsert",
+                        query: account
+                    }
+                }>
+                    <button className={styles.btnEdit}>
+                        <i className="fas fa-edit">Edit</i>
+                    </button>
+                </Link>
+
             </td>
         </tr >
     )
